4125d51c8a8ad6ccf55635ffe645bb2031f16e04
amir.darvishi <amir.darvish@gmail.com>
2024-06-29 17:18:03 +0200

    Add main file and write some comments to know about procedure that I will do in next steps

---
+++ main.py
@@ -0,0 +1,18 @@
+#imports area 
+
+#source 
+def main():
+    # Load the data
+    
+    # Check data quality
+    
+    # Preprocess the data
+    
+    
+    # Analyze word frequencies
+    
+    # Save the results to a CSV file
+    return 0
+
+if __name__ == "__main__":
+    main()
